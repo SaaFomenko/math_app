e50ed98b5bdf963f6678c3a9246fd65456248728
SaaFomenko <s.fomenko@list.ru>
2023-05-14 01:23:53 +0300

    Do not make app, need fix error wiht linking

--- main.cpp
+++ main.cpp
@@ -1,16 +1,39 @@
 #include <iostream>
-//#include "lib/math_fn.h"
+#include "lib/str_split/str_split.h"
+#include "lib/math_fn/math_fn.h"
 
 
 int main()
 {
+	const std::string delimeter = ", ";
+
 	std::string user_in = "";
 
-	std::cout << "[IN]: ";
-
-	getline(std::cin, user_in);
-
-	std::cout << "[OUT]: " << user_in << std::endl;
+	while(true)
+	{
+		std::cout << "[IN]: ";
+  
+		getline(std::cin, user_in);
+  
+		//	std::cout << "[OUT]: " << user_in << std::endl;
+  
+		if (user_in.find(delimeter) == std::string::npos)
+		{
+			try
+			{
+				int x = std::stoi(user_in);
+				std::cout << "[OUT]: " << math_fn::sqrt<int>(x) << std::endl;
+			}
+			catch (std::exception e)
+			{
+				std::cout << e.what() << std::endl;
+			}
+		}
+		else
+		{
+			std::cout << "Функция пока не умеет работать с несколькими значениями.\n";
+		}
+	}
 
 	return 0;
 }
